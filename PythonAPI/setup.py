5aeb4c2436266143dc29b57a2a6b7fb4e0d7e81b
Ahmad Kiswani <kiswani.ahmad@gmail.com>
2022-10-19 00:54:40 +0300

    Fixed numpy linking (#14)
    Don't hardcode paths to numpy headers.

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,21 +1,27 @@
 from setuptools import setup, Extension
 import numpy as np
+from numpy.distutils.misc_util import get_info
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
 
+library_dirs = []
+library_dirs += get_info('npymath')['library_dirs']
+
 ext_modules = [
     Extension(
-        'pycocotools._mask',
+        name='pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        include_dirs = [np.get_include(), '../common'],
     ),
     Extension(
-        'ext',
+        name='ext',
         sources=['pycocotools/ext.cpp', 'pycocotools/simdjson.cpp'],
-        extra_compile_args=['-O3', '-Wall', '-shared', '-fopenmp', '-std=c++17', '-fPIC', '-I/opt/conda/lib/python3.8/site-packages/numpy/core/include', '-Ipycocotools/'],
-        extra_link_args=['-lgomp', '-L/opt/conda/lib/python3.8/site-packages/numpy/core/lib', '-lnpymath'],
+        extra_compile_args=['-O3', '-Wall', '-shared', '-fopenmp', '-std=c++17', '-fPIC'],
+        include_dirs = [np.get_include(), 'pycocotools'],
+        library_dirs=library_dirs,
+        libraries=['npymath', 'gomp']
     )
 ]
 
