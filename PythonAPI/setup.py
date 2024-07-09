323bc9bcddd33e3f6ad23f001f5bcbc486b41508
Nils Bischoff <nib@informatik.uni-kiel.de>
2024-07-09 07:09:23 +0200

    Replace deprecated numpy.distutils

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,26 +1,23 @@
+from os.path import abspath, join
 from setuptools import setup, Extension
 import numpy as np
-from numpy.distutils.misc_util import get_info
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
-
-library_dirs = []
-library_dirs += get_info('npymath')['library_dirs']
 
 ext_modules = [
     Extension(
         name='pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-        include_dirs = [np.get_include(), '../common'],
+        include_dirs=[np.get_include(), '../common'],
     ),
     Extension(
         name='ext',
         sources=['pycocotools/ext.cpp', 'pycocotools/simdjson.cpp'],
         extra_compile_args=['-O3', '-Wall', '-shared', '-fopenmp', '-std=c++17', '-fPIC'],
-        include_dirs = [np.get_include(), 'pycocotools'],
-        library_dirs=library_dirs,
+        include_dirs=[np.get_include(), 'pycocotools'],
+        library_dirs=[abspath(join(np.get_include(), '..', 'lib'))],
         libraries=['npymath', 'gomp']
     )
 ]
@@ -28,12 +25,12 @@ ext_modules = [
 setup(
     name='pycocotools',
     packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
+    package_dir={'pycocotools': 'pycocotools'},
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
         'matplotlib>=2.1.0',
     ],
     version='2.0+nv0.8.0',
-    ext_modules= ext_modules
+    ext_modules=ext_modules
 )
